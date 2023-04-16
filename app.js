60ede308d85b25cb9a2fd394e15d84cf08dafd49
Admin <mistermustache33@gmail.com>
2023-04-16 21:05:58 +0200

    Updated Style

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import {DecisionTree} from "./libraries/decisiontree.js"
 
-const eatbtn = document.querySelector("#consume");
-eatbtn.addEventListener("click", () => loadSavedModel());
+const heartbtn = document.querySelector("#heart");
+heartbtn.addEventListener("click", () => loadSavedModel());
 
 function loadSavedModel() {
     fetch("./model/model.json")
@@ -12,19 +12,20 @@ function loadSavedModel() {
 function modelLoaded(model) {
     let decisionTree = new DecisionTree(model)
 
-    let odor= document.getElementById('odor').value;
-    let capColor= document.getElementById('cap-color').value;
+    let age= document.getElementById('age').value;
+    let cp= document.getElementById('cp').value;
+    let thalach= document.getElementById('thalach').value;
 
-    let mushroom = {odor: odor, capColor : capColor}
-    let prediction = decisionTree.predict(mushroom)
+    let heartChance = {age: age, cp: cp, thalach: thalach}
+    let prediction = decisionTree.predict(heartChance)
     console.log("Predicted: " + prediction)
 
-    if (prediction === "p") {
-        let element = document.getElementById("mushroom")
-        element.innerText = `You have consumed a poisonous mushroom`
+    if (prediction == 1) {
+        let element = document.getElementById("heartAttack")
+        element.innerText = `You have a higher chance of having a heart attack! Be careful.`
     } else {
-        let element = document.getElementById("mushroom")
-        element.innerText = `You have consumed an edible mushroom`
+        let element = document.getElementById("heartAttack")
+        element.innerText = `You have a healthy heart, low chance of getting a heart attack!`
     }
 
 }
